3f492e42b4e97411cd14051810b70c5c077332be
Leo-Zh9 <leo.zhang@outlook.com>
2025-09-24 04:30:45 -0400

    Full Next.js page.tsx with waves + Spotify integration

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,13 @@
-import React, { useEffect } from "react";
+import React from "react";
+import Script from "next/script";
 
 const HomePage = () => {
-  useEffect(() => {
-    import("../public/script.js");
-  }, []);
-
   return (
     <div>
+      {/* Waves Canvas */}
       <canvas id="waveCanvas"></canvas>
 
+      {/* Spotify Track Info */}
       <div id="spotify-container">
         <h1 id="track-name">Loading...</h1>
         <h2 id="track-artist"></h2>
@@ -21,7 +20,11 @@ const HomePage = () => {
         />
       </div>
 
+      {/* Load your CSS */}
       <link rel="stylesheet" href="/style.css" />
+
+      {/* Load your JS */}
+      <Script src="/script.js" strategy="afterInteractive" />
     </div>
   );
 };
