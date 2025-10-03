5a420d5e2270d365943742b7aeb30b99b6e6b59d
Leo-Zh9 <leo.zhang@outlook.com>
2025-10-02 20:55:04 -0400

    Temporarily disable Next.js image optimization
    - Set unoptimized: true to bypass image optimization
    - This should fix 400 errors from /_next/image routes
    - Images will load directly from public folder

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     minimumCacheTTL: 60,
     dangerouslyAllowSVG: true,
-    unoptimized: false,
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
